54be6296c6493e0d7da217e330cf38a9c8341542
Louis-Clement Schiltz <me@louis-clement.com>
2016-02-23 15:12:31 +0100

    created follows form

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,23 @@
 class FollowsController < ApplicationController
+
+  def new
+    @follow = Follow.new
+  end
+
+  def create
+    @follow = Follow.new(follow_params)
+    if @follow.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def competitor_params
+    params.require(:competitor).permit(:user_id, :competitor_id)
+  end
+
+
 end
