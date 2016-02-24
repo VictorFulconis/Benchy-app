0aff5db18236ecc5dcd890f71d0dc91e63f11b13
Edgar Chaput <chaput.edgar@gmail.com>
2016-02-24 15:33:34 +0100

    correct nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    if user_signed_in?
+      redirect_to ads_path
+    else
+    end
   end
 end
+
