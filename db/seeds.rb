53908bf8cfeb81c4154c4a3215a3715e898584a6
victor fulconis <victor.fulconis@gmail.com>
2016-02-25 17:16:51 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -129,7 +129,7 @@ follow.save
 images_url = ["k1rb2hq1sh8jqkosvlyi", "duj3gymo2hdseug0ipp5", "dmas2cpdkoyul4o5edhk"]
 images_url.each do |image|
   Ad.create(
-    url: image,
+    cloudinary_public_id: image,
     competitor_id: competitor.id
     )
 end
@@ -147,7 +147,7 @@ follow.save
 images_url = ["xrstfgeajkfiixvpc96w", "usxmzix6pd227l015yvc", "kdzkhmbbew68icdk2dhq"]
 images_url.each do |image|
   Ad.create(
-    url: image,
+    cloudinary_public_id: image,
     competitor_id: competitor.id
     )
 end
@@ -165,7 +165,7 @@ follow.save
 images_url = ["xq8xikstorlorfqtfw7e", "emqkw3er7sttudrvwpqw", "sxnod6hxgmtyzyy0xrbz"]
 images_url.each do |image|
   Ad.create(
-    url: image,
+    cloudinary_public_id: image,
     competitor_id: competitor.id
     )
 end
@@ -183,7 +183,7 @@ follow.save
 images_url = ["w0sxnslu8eanvnzckzr4", "zrlvikuofxbzd6kisr6t", "jw8u1zz2khhokqp5owzh"]
 images_url.each do |image|
   Ad.create(
-    url: image,
+    cloudinary_public_id: image,
     competitor_id: competitor.id
     )
 end
