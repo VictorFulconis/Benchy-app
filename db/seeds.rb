754e289182667f495cb0b35b805b5b4b796efb8a
victor fulconis <victor.fulconis@gmail.com>
2016-02-25 17:10:48 +0100

    improved seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,3 +101,95 @@ Ad.first(4).each do |ad|
     )
 end
 
+user = User.new(
+  email: "b@gmail.com",
+  password: "aaaabbbb",
+  first_name: "Bob",
+  last_name: "Morane",
+  company_url: "www.ebay.com",
+  company_name: "Ebay"
+  )
+user.save
+dashboard = Dashboard.new(
+  name: "My first dashboard",
+  user_id: user.id
+  )
+dashboard.save
+competitor = Competitor.new(
+  name: "amazon",
+  url: "www.amazon.fr",
+  scraping_parameters: ""
+  )
+competitor.save
+follow = Follow.new(
+  user_id: user.id,
+  competitor_id: competitor.id
+  )
+follow.save
+images_url = ["k1rb2hq1sh8jqkosvlyi", "duj3gymo2hdseug0ipp5", "dmas2cpdkoyul4o5edhk"]
+images_url.each do |image|
+  Ad.create(
+    url: image,
+    competitor_id: competitor.id
+    )
+end
+competitor = Competitor.new(
+  name: "cdiscount",
+  url: "www.cdiscount.fr",
+  scraping_parameters: ""
+  )
+competitor.save
+follow = Follow.new(
+  user_id: user.id,
+  competitor_id: competitor.id
+  )
+follow.save
+images_url = ["xrstfgeajkfiixvpc96w", "usxmzix6pd227l015yvc", "kdzkhmbbew68icdk2dhq"]
+images_url.each do |image|
+  Ad.create(
+    url: image,
+    competitor_id: competitor.id
+    )
+end
+competitor = Competitor.new(
+  name: "fnac",
+  url: "www.fnac.fr",
+  scraping_parameters: ""
+  )
+competitor.save
+follow = Follow.new(
+  user_id: user.id,
+  competitor_id: competitor.id
+  )
+follow.save
+images_url = ["xq8xikstorlorfqtfw7e", "emqkw3er7sttudrvwpqw", "sxnod6hxgmtyzyy0xrbz"]
+images_url.each do |image|
+  Ad.create(
+    url: image,
+    competitor_id: competitor.id
+    )
+end
+competitor = Competitor.new(
+  name: "priceminister",
+  url: "www.priceminister.fr",
+  scraping_parameters: ""
+  )
+competitor.save
+follow = Follow.new(
+  user_id: user.id,
+  competitor_id: competitor.id
+  )
+follow.save
+images_url = ["w0sxnslu8eanvnzckzr4", "zrlvikuofxbzd6kisr6t", "jw8u1zz2khhokqp5owzh"]
+images_url.each do |image|
+  Ad.create(
+    url: image,
+    competitor_id: competitor.id
+    )
+end
+Ad.first(4).each do |ad|
+  Pin.create(
+    ad_id: ad.id,
+    dashboard_id: dashboard.id
+    )
+end
