f10ead524749f9f82a4c78e190db5e7232ac797b
victor fulconis <victor.fulconis@gmail.com>
2016-02-29 15:06:47 +0100

    2nd round

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,7 +2,10 @@ class SharesController < ApplicationController
 
   def create
     params[:share][:user_id].each do |user_id|
-      @share = Share.new(dashboard_id: params[:dashboard_id], user_id: user_id)
+      if user_id != ""
+        @share = Share.new(dashboard_id: params[:dashboard_id], user_id: user_id)
+        @share.save
+      end
     end
     redirect_to dashboard_path(params[:dashboard_id]), notice: "The dashboard has been shared, your team mates will be warned shortly"
   end
