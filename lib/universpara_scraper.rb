f494befa7209dd79cb738faa11cffa6173d41385
Louis-Clement Schiltz <me@louis-clement.com>
2016-03-01 18:06:16 +0100

    added UniversPara Scraper

---
+++ lib/universpara_scraper.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+
+# Competitor.all.each do |competitor|
+
+# html_doc.search("#{competitor.scraping_parameters}").each do |element|
+#   curl element.img.url > element.img.name.jpg
+# end
+
+class UniversParaScraper
+  def initialize(competitor)
+    @competitor = competitor
+  end
+
+  def run
+    html_file = open("http://#{@competitor.url}", :allow_redirections => :all)
+    html_doc = Nokogiri::HTML(html_file)
+
+    html_doc.search(".homeitems img").each do |element|
+      response = Cloudinary::Uploader.upload(element.attr('src'))
+      @competitor.ads.create(cloudinary_public_id: response['public_id'])
+    end
+  end
+end
